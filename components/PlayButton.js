42294f4ff2c34e689764d684df2e262bc302cfe8
ceessay <mceessay@gmail.com>
2018-11-12 00:54:12 +0000

    customized video

--- components/PlayButton.js
+++ components/PlayButton.js
@@ -1,41 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
-import Icons from 'react-native-vector-icons/MaterialIcons'
-
-const backgroundColor = 'transparent'
+import React from "react";
+import PropTypes from "prop-types";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import Icons from "react-native-vector-icons/MaterialIcons";
 
 const styles = StyleSheet.create({
   playButton: {
     opacity: 0.9
   },
+
+  playWrapper: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    width: 60,
+    borderRadius: 75 / 2
+  },
   playContainer: {
     flex: 1,
-    backgroundColor,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   }
-})
+});
 
 const PlayButton = props => (
-  <View style={styles.playContainer}>
+  <View style={[styles.playContainer]}>
     <TouchableOpacity
       onPress={() => props.onPress()}
+      style={[styles.playWrapper, { backgroundColor: props.theme }]}
     >
       <Icons
         style={styles.playButton}
-        name={props.paused ? 'play-circle-outline' : 'pause-circle-outline'}
-        color={props.theme}
-        size={75}
+        name={props.paused ? "play-arrow" : "pause"}
+        color={"white"}
+        size={40}
       />
     </TouchableOpacity>
   </View>
-)
+);
 
 PlayButton.propTypes = {
   onPress: PropTypes.func.isRequired,
   paused: PropTypes.bool.isRequired,
   theme: PropTypes.string.isRequired
-}
+};
 
-export { PlayButton }
+export { PlayButton };
